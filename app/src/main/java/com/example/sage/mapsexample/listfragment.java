6111f617acbd65754e92433e841943703cf2fb0b
rkinabhi <rkinabhi@gmail.com>
2018-01-24 15:31:37 +0530

    list of groups

--- app/src/main/java/com/example/sage/mapsexample/listfragment.java
+++ app/src/main/java/com/example/sage/mapsexample/listfragment.java
@@ -3,24 +3,40 @@ package com.example.sage.mapsexample;
 /**
  * Created by sage on 13/1/18.
  */
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class listfragment extends Fragment{
     private ListView listGroups;
     private Button switchButon;
+    String[] groupsArray;
+    private String name;
+    private String Uid;
 
 
 
@@ -41,21 +57,74 @@ public class listfragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.listfregment,container,false);
+        View view = inflater.inflate(R.layout.list_fragment,container,false);
+        listGroups = view.findViewById(R.id.listView2);
+        switchButon = view.findViewById(R.id.switchButon);
+//        name = ;
+        Uid = mAuth.getUid();
 
-       // db.getReference("/Details/"+ mAuth.getUid() + "/Group ").ad
+        db = FirebaseDatabase.getInstance();
+        db.getReference("Details/"+Uid+"Name").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                name = dataSnapshot.getValue(String.class);
+                DatabaseReference groupsReference = db.getReference("Details/"+Uid+"/Group");
+                groupsReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        final HashMap<String, String> groupsMap = new HashMap<>();
+                        for(DataSnapshot d : dataSnapshot.getChildren()){
+                            groupsMap.put(d.getValue(String.class), d.getKey());
+                        }
+                        groupsArray = new String[groupsMap.size()];
+                        int i=0;
+                        for(Map.Entry<String, String> a : groupsMap.entrySet()){
+                            groupsArray[i] = a.getKey();
+                            i++;
+                        }
+                        ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(getActivity().getApplicationContext(), android.R.layout.simple_list_item_1, groupsArray){
+                            @Override
+                            public View getView(int position, View convertView, ViewGroup parent) {
+                                View view =super.getView(position, convertView, parent);
+
+                                TextView textView=(TextView) view.findViewById(android.R.id.text1);
+                                textView.setTextColor(Color.BLUE);
+                                return view;
+                            }
+                        };
+                        listGroups.setAdapter(new ArrayAdapter<String>(getActivity().getApplicationContext(), R.layout.list_fragment_item, R.id.list_content, groupsArray));
+
+                        listGroups.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                                TextView tv = view.findViewById(R.id.list_content);
+                                Log.d(TAG, "group name: "+tv.getText().toString());
+                                Log.d(TAG, "groupID: "+groupsMap.get(tv.getText().toString()));
+                                Intent maps = new Intent(getActivity(), MapsActivity.class);
+                                maps.putExtra("UserID", Uid);
+                                maps.putExtra("Name", name);
+                                maps.putExtra("GroupID", groupsMap.get(tv.getText().toString()));
+                                startActivity(maps);
+                            }
+                        });
+                    }
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) { }
+                });
+
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) { }
+        });
 
 
+        switchButon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: "+"switch button clickd");
+            }
+        });
 
-//        switchButon.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//
-//            }
-//        });
-        //listGroups.setAdapter();
-
-        //db.getReference("/Details/")
         return view;
     }
     public void MakeLog(String mesaage){
