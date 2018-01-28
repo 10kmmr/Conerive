2a24662f7e2fd7c9de17bb0a46832a16dbb454a6
prithvihv <hvprithvi09@gmail.com>
2018-01-28 19:28:41 +0530

    splash screen checking auth

--- app/src/main/java/com/example/sage/mapsexample/splashScreen.java
+++ app/src/main/java/com/example/sage/mapsexample/splashScreen.java
@@ -8,32 +8,61 @@ import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class splashScreen extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    FirebaseDatabase database;
     private TextView tv;
     private ImageView iv;
+    private boolean isLoggedin =false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        mAuth = FirebaseAuth.getInstance();
+        database= FirebaseDatabase.getInstance();
         tv = (TextView) findViewById(R.id.splashtxt);
         iv = (ImageView) findViewById(R.id.splashimg);
         Animation myanim = AnimationUtils.loadAnimation(this, R.anim.splashscreen);
         tv.startAnimation(myanim);
         iv.startAnimation(myanim);
-        final Intent main = new Intent(this,MainActivity.class);
-        Thread timer = new Thread() {
-            public void run() {
-                try {
-                    sleep(5000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } finally {
-                    startActivity(main);
-                    finish();
-                }
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            isLoggedin = true;
+            NextActivity();
+        }else{
+            Intent notLoggedin = new Intent(this,MainActivity.class);
+            startActivity(notLoggedin);
+        }
+    }
+
+    public void NextActivity() {
+        final Intent loggedin = new Intent(this, Groupselector.class);
+        database.getReference("Details").child(mAuth.getUid()).child("Name").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                loggedin.putExtra("UserID", mAuth.getUid());
+                loggedin.putExtra("Name", dataSnapshot.getValue().toString());
+                loggedin.putExtra("GroupID", "NULL");
+                startActivity(loggedin);
             }
-        };
-        timer.start();
+            @Override
+            public void onCancelled(DatabaseError databaseError) { }
+        });
     }
 
 }
