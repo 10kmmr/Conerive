d7cafd2b790c889c6f82dfbe7b434d9697dade36
prithvihv <hvprithvi09@gmail.com>
2018-02-11 13:18:22 +0530

    user setting values

--- app/src/main/java/com/example/sage/mapsexample/UserSetting.java
+++ app/src/main/java/com/example/sage/mapsexample/UserSetting.java
@@ -6,8 +6,20 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class UserSetting extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    public EditText nameET;
+    public EditText emailET;
+
+    public Button done;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +36,23 @@ public class UserSetting extends AppCompatActivity {
 //                        .setAction("Action", null).show();
 //            }
 //        });
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
 
+        nameET = (EditText) findViewById(R.id.name);
+        emailET = (EditText) findViewById(R.id.email);
+        done = (Button)findViewById(R.id.done);
+
+
+        String uid = mAuth.getUid();
+        String name= nameET.getText().toString();
+        String email= emailET.getText().toString();
+        done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //HTTP writing
+            }
+        });
     }
 
 }
