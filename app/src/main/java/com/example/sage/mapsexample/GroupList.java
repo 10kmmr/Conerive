235841c23061773831fe642b3d4ac2f91ff2ba70
rkinabhi <rkinabhi@gmail.com>
2018-02-18 22:23:36 +0530

    added list adapter for groups

--- app/src/main/java/com/example/sage/mapsexample/GroupList.java
+++ app/src/main/java/com/example/sage/mapsexample/GroupList.java
@@ -1,8 +1,21 @@
 package com.example.sage.mapsexample;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GroupList extends AppCompatActivity {
 
@@ -12,6 +25,48 @@ public class GroupList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_list);
         groupList = findViewById(R.id.groupList);
+        ListView yourListView =findViewById(R.id.groupList);
+
+        ArrayList<GroupListDataModel> list = new ArrayList<>();
+
+        GroupListAdapter groupListAdapter = new GroupListAdapter(this, R.layout.group_list_item, list);
+        yourListView.setAdapter(groupListAdapter);
+    }
+
+    class GroupListAdapter extends ArrayAdapter<GroupListDataModel> {
+        public GroupListAdapter(Context context, int textViewResourceId) {
+            super(context, textViewResourceId);
+        }
+
+        public GroupListAdapter(Context context, int resource, ArrayList<GroupListDataModel> items) {
+            super(context, resource, items);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            View view = convertView;
+
+            if (view == null) {
+                LayoutInflater vi;
+                vi = LayoutInflater.from(getContext());
+                view = vi.inflate(R.layout.group_list_item, null);
+            }
+
+            GroupListDataModel groupListDataModelItem = getItem(position);
+
+            if (groupListDataModelItem != null) {
+                ImageView groupImageTV = view.findViewById(R.id.groupImage);
+                TextView groupNameTV = view.findViewById(R.id.groupName);
+                TextView memberCountTV = view.findViewById(R.id.memberCount);
+                TextView tripCountTV = view.findViewById(R.id.tripCount);
+                TextView imageCountTV = view.findViewById(R.id.imageCount);
+
+                groupNameTV.setText(groupListDataModelItem.getGroupName());
+            }
+
+            return view;
+        }
 
     }
 }
