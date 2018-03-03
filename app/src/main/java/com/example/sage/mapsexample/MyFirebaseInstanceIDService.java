829993f4b12b8c690584f7216ff4cbe613795038
prithvihv <hvprithvi09@gmail.com>
2018-03-03 09:27:22 +0530

    nnew classes

---
+++ app/src/main/java/com/example/sage/mapsexample/MyFirebaseInstanceIDService.java
@@ -0,0 +1,8 @@
+package com.example.sage.mapsexample;
+
+/**
+ * Created by hvpri on 03-03-2018.
+ */
+
+public class MyFirebaseInstanceIDService {
+}
---
+++ app/src/main/java/com/example/sage/mapsexample/MyFirebaseMessagingService.java
@@ -0,0 +1,17 @@
+package com.example.sage.mapsexample;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+/**
+ * Created by hvpri on 03-03-2018.
+ */
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+
+}
