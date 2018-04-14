49d77154c6b923d8ea15bf34c815c5954447c5ef
abhijeeth padarthi <rkinabhi@gmail.com>
2018-04-15 03:01:13 +0530

    new structure.... gg...

--- app/src/main/java/com/example/sage/mapsexample/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/sage/mapsexample/MyFirebaseMessagingService.java
@@ -112,7 +112,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
      * @param messageBody FCM message body received.
      */
     private void sendNotification(String messageBody) {
-        Intent intent = new Intent(this, HomeActivity.class);
+        Intent intent = new Intent(this, SplashScreenActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
                 PendingIntent.FLAG_ONE_SHOT);
--- app/src/main/java/com/example/sage/mapsexample/UserSettingsActivity.java
+++ app/src/main/java/com/example/sage/mapsexample/UserSettingsActivity.java
@@ -3,11 +3,11 @@ package com.example.sage.mapsexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class HomeActivity extends AppCompatActivity {
+public class UserSettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_user_settings);
     }
 }
