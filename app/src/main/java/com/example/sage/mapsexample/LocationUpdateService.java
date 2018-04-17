85a88f11f762f24bcda0eff27d9e9eed41d24589
abhijeeth padarthi <rkinabhi@gmail.com>
2018-04-17 23:56:27 +0530

    written service

--- app/src/main/java/com/example/sage/mapsexample/LocationUpdateService.java
+++ app/src/main/java/com/example/sage/mapsexample/LocationUpdateService.java
@@ -6,7 +6,6 @@ import android.location.Location;
 import android.os.IBinder;
 import android.os.Looper;
 import android.util.Log;
-
 import com.firebase.geofire.GeoFire;
 import com.firebase.geofire.GeoLocation;
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -14,6 +13,10 @@ import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LocationUpdateService extends Service {
     private static final String TAG = "LocationUpdateService";
@@ -22,7 +25,13 @@ public class LocationUpdateService extends Service {
     private Location mLastKnownLocation;
     private LocationRequest mLocationRequest;
     private LocationCallback mLocationCallback;
-    GeoFire ownerGeoFireObject;
+
+    private DatabaseReference ownerReference;
+    private FirebaseDatabase database;
+    private GeoFire ownerGeoFireObject;
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser currentUser;
 
     public LocationUpdateService() {
     }
@@ -37,8 +46,11 @@ public class LocationUpdateService extends Service {
     public void onCreate() {
         super.onCreate();
 
-//        ownerReference = database.getReference("Root/"+groupID+"/"+userID);
-//        ownerGeoFireObject = new GeoFire(ownerReference);
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+        database = FirebaseDatabase.getInstance();
+        ownerReference = database.getReference("USERS/"+currentUser.getUid());
+        ownerGeoFireObject = new GeoFire(ownerReference);
 
         mLocationCallback = new LocationCallback(){
             @Override
@@ -46,7 +58,7 @@ public class LocationUpdateService extends Service {
                 for (Location location : locationResult.getLocations()) {
                     Log.i(TAG, "Location: " + location.getLatitude() + " " + location.getLongitude());
                     mLastKnownLocation=location;
-//                    ownerGeoFireObject.setLocation("Location", new GeoLocation(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude()));
+                    ownerGeoFireObject.setLocation("Location", new GeoLocation(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude()));
 
                 }
             }
@@ -56,6 +68,7 @@ public class LocationUpdateService extends Service {
         mLocationRequest.setInterval(2000);
         mLocationRequest.setFastestInterval(2000);
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
         mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
     }
 
